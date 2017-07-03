22fe7465e747b21934d6c386bfc397481bbeba89
hsin <shinsonh@gmail.com>
2017-07-02 22:35:54 -0400

    add section right

--- app/main.js
+++ app/main.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import styles from './styles.css';
 import hero from './assets/hero-img.png';
 import logo from './assets/hero-menu-logo.png';
+import doc1 from './assets/docs/doc1.png';
 
 class App extends React.Component {
   constructor(props) {
@@ -60,14 +61,19 @@ class App extends React.Component {
             聯絡我們
           </div>
         </div>
-        <div className={styles.inputGroup}>
+        {/*<div className={styles.inputGroup}>
           <div style={{display: 'inline-flex'}}>
-            {/*<input placeholder="join@nltr.tw" type="email" onChange={this.handleInput} value={this.state.inputValue} className={styles.emailInput} />
-            <div className={styles.button} onClick={this.handleSubmit}>Submit </div>*/}
+            <input placeholder="join@nltr.tw" type="email" onChange={this.handleInput} value={this.state.inputValue} className={styles.emailInput} />
+            <div className={styles.button} onClick={this.handleSubmit}>Submit </div>
           </div>
           <p style={{color: '#D0011B'}}> {this.state.message} </p>
-        </div>
+        </div>*/}
         <img src={hero} className={styles.taiwan} />
+        <div className={styles.sectionRight}>
+          <h4 className={styles.sectionTitle}>歷史是屬於你我、屬於每一個人民的。</h4>
+          <p>關於台灣過去的拼圖，散落在全世界的各個角落。我們需要把它們找出來、加以數位化，轉譯成為人人都可取用的開放資料與有意義的歷史故事；在拼湊這些線索的過程中，讓台灣人民得以認識自身的歷史與真相。</p>
+          <img src={doc1} />
+        </div>
       </div>
     );
   }
