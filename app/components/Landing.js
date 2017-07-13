ffaab9ea863fc30d4bfb29b8b349146fdd85a73e
hsin <shinsonh@gmail.com>
2017-07-13 16:00:37 -0400

    Add nav header and scroll fade in effect on docs

--- app/components/Landing.js
+++ app/components/Landing.js
@@ -5,6 +5,7 @@ import styles from '../styles.css';
 import hero from '../assets/hero-img.png';
 import logo from '../assets/hero-menu-logo.png';
 import mobileLogo from '../assets/mobile-menu-logo.png';
+import navLogo from '../assets/nav-logo.png';
 import doc1 from '../assets/docs/doc1.png';
 import doc2 from '../assets/docs/doc2.png';
 import doc3 from '../assets/docs/doc3.png';
@@ -17,9 +18,17 @@ export default class Landing extends React.Component {
     this.state = {
       inputValue: '',
       message: '',
+      scrollTop: 0,
     };
   }
 
+  componentDidMount() {
+     window.addEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    this.setState({ scrollTop: document.body.scrollTop || document.documentElement.scrollTop });
+  }
 
   handleInput = (e) => {
     this.setState({
@@ -50,8 +59,34 @@ export default class Landing extends React.Component {
   }
 
   render() {
+    const { scrollTop } = this.state;
+    const easing = (startPosition, endPosition) => {
+      if (startPosition > endPosition || scrollTop > endPosition) {
+        return 1;
+      } else if (scrollTop < startPosition) {
+        return 0;
+      }
+      return 1 - ((endPosition - scrollTop) / (endPosition - startPosition));
+    }
     return (
       <div className={styles.box}>
+        <div className={styles.navBar} style={{ opacity: easing(481, 560) }}>
+          <img src={navLogo} className={styles.navLogo} />
+          <div className={styles.navHeader}>
+            <div className={styles.navHeaderItem} >
+              參與尋寶
+            </div>
+            <div className={styles.navHeaderItem} >
+              寶藏庫
+            </div>
+            <div className={styles.navHeaderItem} >
+              故事
+            </div>
+            <div className={styles.navHeaderItem} >
+              聯絡我們
+            </div>
+          </div>
+        </div>
         <img src={logo} className={styles.logo} />
         <img src={mobileLogo} className={styles.mobileLogo} />
         <div className={styles.navBox}>
@@ -79,10 +114,10 @@ export default class Landing extends React.Component {
         <div className={styles.sectionRight}>
           <h4 className={styles.sectionTitle}>歷史是屬於你我、屬於每一個人民的。</h4>
           <p className={styles.sectionItem}>關於台灣過去的拼圖，散落在全世界的各個角落。我們需要把它們找出來、加以數位化，轉譯成為人人都可取用的開放資料與有意義的歷史故事；在拼湊這些線索的過程中，讓台灣人民得以認識自身的歷史與真相。</p>
-          <img src={doc1} className={styles.doc1Img} />
+          <img src={doc1} className={styles.doc1Img} style={{ opacity: easing(0, 600) - 0.2, /* marginTop: 100 * (1 - easing(0, 600)) */ }} />
         </div>
         <div className={styles.sectionLeft}>
-          <img src={doc2} className={styles.doc2Img} />
+          <img src={doc2} className={styles.doc2Img} style={{ opacity: easing(461, 968) - 0.2 }} />
           <div className={styles.sectionLeftContent} >
             <h4 className={styles.sectionTitle}>為什麼是國家寶藏？</h4>
             <p className={styles.sectionItemNarrow} >包括英美等國家這一百多年以來對台灣社會、地理、人物所做下的情蒐紀錄，提供了台灣人在回顧自己的過去時，更豐富的材料與角度，以及不同的思維，去重新描繪台灣人曾經的生活面貌。
@@ -120,7 +155,7 @@ export default class Landing extends React.Component {
           </div>
         </div>
         <div className={styles.gallerySection}>
-          <img src={doc3} className={styles.doc3Img} />
+          <img src={doc3} className={styles.doc3Img} style={{ opacity: easing(1600, 2100) - 0.2 }} />
           <img src={fish} className={styles.fishImg} />
           <div className={styles.footer}>
             <div className={styles.footerContent}>
